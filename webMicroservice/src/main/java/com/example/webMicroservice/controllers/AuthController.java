f3e8cd84209d186e5509095e77eee1cafbc14b72
aleksandrbreusov <alexsbreusov@gmail.com>
2024-06-10 23:43:50 +0300

    fix: correct cat reading error

--- webMicroservice/src/main/java/com/example/webMicroservice/controllers/AuthController.java
+++ webMicroservice/src/main/java/com/example/webMicroservice/controllers/AuthController.java
@@ -16,9 +16,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
-import java.util.Optional;
-import java.util.Set;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/auth")
